1ea7a7c3c89e0f6437653e6394a06a5099e841de
DiegoRamirezFuente <diego.ramirez.fuente@alumnos.upm.es>
2024-06-21 17:54:11 +0200

    Velocidad 0

--- SPEED_TEST.srcs/sources_1/new/pulse_counter.vhd
+++ SPEED_TEST.srcs/sources_1/new/pulse_counter.vhd
@@ -1,17 +1,17 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
+use IEEE.numeric_std.ALL;
 
 entity pulse_counter is
   Generic (
-    SAMPLES : INTEGER range 1 to 10 := 5; -- numero de muestras que se toman para hacer la media
-    FREC : INTEGER range 0 to 1e9 := 1000000 -- frecuencia de conteo entre 10Mhz y 1Ghz
+    SAMPLES : INTEGER range 1 to 10 := 5; -- número de muestras que se toman para hacer la media
+    FREC : INTEGER range 0 to 1e9 := 100000000 -- frecuencia de conteo entre 10MHz y 1GHz
   );
   Port ( 
     CLK   : in std_logic;
     RESET : in std_logic;
-    PULSE : in STD_LOGIC; -- pulso entrante procedente del sensor hall
-    RPM : out std_logic_vector(19 downto 0)
+    PULSE : in std_logic; -- pulso entrante procedente del sensor hall
+    RPM : out std_logic_vector(19 downto 0) 
   );
 end pulse_counter;
 
@@ -25,15 +25,14 @@ architecture Behavioral of pulse_counter is
 
   signal Flag : std_logic := '0';
 
-  constant MAX : std_logic_vector(32 downto 0) := (others => '1');
+  constant MAX : INTEGER := 200000000; -- valor máximo del contador
 
   signal total : INTEGER := 0; -- almacena el sumatorio de las cuentas para hallar la media
   signal avg_count : INTEGER := 0; -- media de pulsos contados
-  signal count_per_rev : integer := 0;
-  signal count_per_rev_frec : integer := 0;
-  signal time_per_rev : integer := 0;
-  signal rpm_v : integer := 0;
-  
+  signal count_per_rev : INTEGER := 0;
+  signal count_per_rev_frec : INTEGER := 0;
+  signal rpm_v : INTEGER := 0;
+
 begin
 
   counter_time: process(CLK, RESET)
@@ -55,10 +54,12 @@ begin
             counter <= 0;
             Flag <= '1';
             pulse_count <= pulse_count + 1;
-          elsif counter = to_integer(unsigned(MAX)) then
-            avg_count <= to_integer(unsigned(MAX));
-            total <= 0;
-            pulse_count <= 0;
+          elsif counter >= MAX then
+              counter <= 0;
+              pulse_count <= 0;
+              total <= 0;
+              avg_count <= 0;
+              rpm_v <= 0;
           else
             counter <= counter + 1;
             Flag <= PULSE; -- Con esto se resetea el valor de flag permitiendo volver a detectar el flanco.
@@ -66,6 +67,8 @@ begin
 
           if pulse_count >= SAMPLES then
             next_state <= CALCULATE_S0;
+          else
+            next_state <= STOP;
           end if;
 
         when CALCULATE_S0 =>
@@ -74,7 +77,7 @@ begin
           next_state <= CALCULATE_S1;
 
         when CALCULATE_S1 =>
-          count_per_rev <= avg_count*4;
+          count_per_rev <= avg_count * 4;
           next_state <= CALCULATE_S2;
 
         when CALCULATE_S2 =>
